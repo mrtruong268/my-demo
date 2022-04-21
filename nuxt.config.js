1497a1ba302a45f95ed026e96c45b1924847b265
mrtruong268 <truongngoan97@gmail.com>
2022-04-21 09:31:05 +0700

    code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 import i18n from './config/i18n'
 
 export default {
-    // target: 'static',
+    target: 'static',
     // router: {
-    //     base: '/my-demo',
+        base: '/my-demo/',
     // },
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
